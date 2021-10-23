f026e8dd06329dda333889409963093ccd17239c
bug1vuge <malutinm11@gmail.com>
2021-10-23 12:29:27 +0600

    исправил ошибки

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const todoCompleted = document.querySelector('.todo-completed');
 const todoRemove = document.querySelectorAll('.todo-remove');
 
 
-let todoData = [];
+let todoData = JSON.parse(localStorage.getItem('array')) || [];
 
 const render = function () {
     todoList.innerHTML = '';
@@ -24,24 +24,31 @@ const render = function () {
         li.setAttribute('id', index);
 
         if (item.completed) {
-            todoCompleted.append(li);
+            todoCompleted.prepend(li);
         } else {
-            todoList.append(li);
+            todoList.prepend(li);
         }
 
         li.querySelector('.todo-complete').addEventListener('click', function () {
             item.completed = !item.completed;
+            localStorage.setItem('array', JSON.stringify(todoData));
             render();
         });
 
         li.querySelector('.todo-remove').addEventListener('click', function () {
             if (index == li.getAttribute('id')) {
                 todoData.splice(index, 1);
+                localStorage.setItem('array', JSON.stringify(todoData));
                 render();
             }
         })
     });
 };
+
+if (todoData) {
+    render();
+}
+
 
 todoControl.addEventListener('submit', function (event) {
 
